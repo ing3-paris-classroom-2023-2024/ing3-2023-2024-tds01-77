36a604c19a24bd157a70c8e66a4cfebffa4a13ee
ZergOvermind <tom.delaye@outlook.fr>
2023-11-01 12:45:25 +0100

    Tom

--- principal/source.c
+++ principal/source.c
@@ -1,13 +1,29 @@
 #include "header.h"
 
-int main(){
+int main() {
+    char *x[3];
+    char y[6];
+    int i,j;
+    i = 0;
+    j = 0;
+
     char *niveau = "niveaux/niveau1.txt";
     FILE *fp = fopen(niveau, "r");
-    char ch;
-    while ((ch = fgetc(fp)) != EOF)
-        putchar(ch);
 
-    // close the file
+    char ch;
+    while ((ch = fgetc(fp)) != EOF) {
+        if (ch!=10){
+            y[i]=ch;
+            i++;
+            printf("%s",y);
+        }
+        if (ch=10){
+            i=0;
+            x[j]=y;
+        }
+    }
+    printf("%s",y);
+
     fclose(fp);
     return 0;
 }
