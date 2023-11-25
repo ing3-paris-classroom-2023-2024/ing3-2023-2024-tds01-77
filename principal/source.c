d2d6d12e2a8ae6428f341f65b7dc8adc9a6e4349
ZergOvermind <tom.delaye@outlook.fr>
2023-11-25 21:15:24 +0100

    Tom

--- principal/source.c
+++ principal/source.c
@@ -32,7 +32,7 @@ void longueur(char *lvl, int *adrlignes, int *adrcolonnes){     ///obtient le nb
 }
 
 
-void import(char *lvl, int *lignes, int *colonnes, char tableau[*lignes][*colonnes], int snoopyXY[2], int *oiseaux){   ///import du fichier et insertion des caractères dans un tableau
+void import(char *lvl, int *lignes, int *colonnes, char tableau[*lignes][*colonnes], int snoopyXY[2], int oiseaux[2]){   ///import du fichier et insertion des caractères dans un tableau
     int i, j;
     i=0;
     j=0;
@@ -42,9 +42,10 @@ void import(char *lvl, int *lignes, int *colonnes, char tableau[*lignes][*colonn
     FILE *fp = fopen(niveau, "r");
 
     char ch;
-    snoopyXY[0] = fgetc(fp) - 64;
-    snoopyXY[1] = fgetc(fp) - 64;
-    *oiseaux = fgetc(fp) - 64;
+    snoopyXY[0] = fgetc(fp) - 65;
+    snoopyXY[1] = fgetc(fp) - 65;
+    oiseaux[0] = fgetc(fp) - 65;
+    oiseaux[1] = fgetc(fp) - 65;
 
     while ((ch = fgetc(fp)) != EOF) {
         if (ch!=10){
@@ -60,6 +61,7 @@ void import(char *lvl, int *lignes, int *colonnes, char tableau[*lignes][*colonn
     fclose(fp);
 }
 
+
 void regles(){
     system("cls");
 
@@ -73,6 +75,7 @@ void regles(){
     getch();
 }
 
+
 void menu(){
     system("cls");
 
@@ -85,13 +88,14 @@ void menu(){
 }
 
 
-void renvoi_sp(int bloc, int snoopyXY[2], int move[2], int *lignes, int *colonnes, char tableau[*lignes][*colonnes]){   ///exécute le script corresondant au bloc rencontré
+void renvoi_sp(int bloc, int snoopyXY[2], int move[2], int *lignes, int *colonnes, char tableau[*lignes][*colonnes], int oiseaux[2]){   ///exécute le script corresondant au bloc rencontré
     switch (bloc){
         case 245:    ///oiseau
-            gotoligcol(20,5);
-            snoopyXY[0] = move[0];
+            gotoligcol(20,5);   ///place le curseur aux coordonnées données (20,5)
+            snoopyXY[0] = move[0];      ///change les coordonnées de snoopy pour qu'ils prennent celles du déplacement demandé
             snoopyXY[1] = move[1];
-            tableau[move[0]][move[1]]=' ';
+            tableau[move[0]][move[1]]=' ';  ///remplace le caractère de l'oiseau par un espace dans le niveau
+            oiseaux[0]+=1;                  ///ajoute 1 au compteur du score
             printf("bird catched");
             break;
 
@@ -120,11 +124,13 @@ void renvoi_sp(int bloc, int snoopyXY[2], int move[2], int *lignes, int *colonne
 }
 
 
-void save(int *lignes, int *colonnes, char tableau1[*lignes][*colonnes], int snoopyXY1[2]){
+void save(int *lignes, int *colonnes, char tableau1[*lignes][*colonnes], int snoopyXY1[2], int oiseaux[2]){
     FILE *fichier = fopen("niveaux/sauvegarde.txt", "w");
 
-    fputc(snoopyXY1[0]+64,fichier);
-    fputc(snoopyXY1[1]+64,fichier);
+    fputc(snoopyXY1[0]+65,fichier);
+    fputc(snoopyXY1[1]+65,fichier);
+    fputc(oiseaux[0]+65,fichier);
+    fputc(oiseaux[1]+65,fichier);
     for (int i=0 ; i<*lignes ; i++){
         for (int j=0 ; j<*colonnes ; j++){
             fputc(tableau1[i][j],fichier);
@@ -140,6 +146,7 @@ void jeu(int lvl){
     system("cls");
 
     char *ListeNiveaux[] = {"niveaux/sauvegarde.txt","niveaux/niveau1.txt", "niveaux/niveautest.txt"};
+    int nb_niveaux = sizeof(ListeNiveaux)/sizeof(ListeNiveaux[0]);
     int lignes, colonnes;
     char *niveau = ListeNiveaux[lvl];
 
@@ -151,14 +158,13 @@ void jeu(int lvl){
     char tableau[lignes][colonnes];     ///définition du tableau en fonction des données fournies par longueur
     int snoopyXY[2]={1,1};      ///coordonnées de snoopy
     int move[2];                        ///coordonnées de snoopy après déplacement
-    int oiseaux=0;
+    int oiseaux[2]={0,0};
 
 
-    import(niveau, &lignes, &colonnes, tableau, snoopyXY, &oiseaux);    ///place le fichier à ouvrir dans tableau
+    import(niveau, &lignes, &colonnes, tableau, snoopyXY, oiseaux);    ///place le fichier à ouvrir dans tableau
 
     move[0]=snoopyXY[0];
     move[1]=snoopyXY[1];
-
 
     bool rencontre;                     ///true si bloc rencontré au mouvement initialisé
 
@@ -169,7 +175,21 @@ void jeu(int lvl){
 
     while (entree != 'q'){
         gotoligcol(0,5);
-        printf("%d oiseaux",oiseaux);
+        printf("%d oiseaux sur %d",oiseaux[0],oiseaux[1]);
+
+        if(oiseaux[0]==oiseaux[1]){
+            system("cls");
+            printf("BRAVOOOOO, vous avez eu tous les oiseaux %d %d",lvl,nb_niveaux);
+            if (lvl+1 >= nb_niveaux){
+                printf("    vous avez fini le jeu");
+            }
+
+            getch();
+            getch();
+            if (lvl+1 < nb_niveaux){
+                jeu(lvl+1);
+            }
+        }
 
         switch(entree){                 ///vérifie si z,q,s ou d est entré par l'utilisateur
             case 72:
@@ -185,7 +205,7 @@ void jeu(int lvl){
                 move[1]+=1;
                 break;
             case 's':
-                save(&lignes, &colonnes, tableau,snoopyXY);
+                save(&lignes, &colonnes, tableau, snoopyXY, oiseaux);
                 break;
 
         }
@@ -196,7 +216,7 @@ void jeu(int lvl){
         for (int i=0 ; blocs[i] != 'f' ; i++){
             if (destination == blocs[i]){   ///si un bloc spécial est rencontré
                 rencontre = true;
-                renvoi_sp(blocs[i], snoopyXY, move, &lignes, &colonnes, tableau);
+                renvoi_sp(blocs[i], snoopyXY, move, &lignes, &colonnes, tableau, oiseaux);
                 move[0]=snoopyXY[0];
                 move[1]=snoopyXY[1];
             }
@@ -226,6 +246,18 @@ void jeu(int lvl){
 }
 
 
+void code(){
+    char entree[10];
+    system("cls");
+    printf("entrez le code :");
+    scanf("%s",entree);
+    if (strcmp(entree,"aab")==0){
+        jeu(1);
+    }
+    printf("\n\ncode invalide, appuyez sur une touche pour retourner au menu");
+    getch();
+}
+
 
 int main() {
     SetConsoleCP(CP_WINANSI);       ///définir l'encodage du projet
@@ -239,7 +271,7 @@ int main() {
     SetConsoleCursorInfo(consoleHandle, &info);
 ///pour cacher les curseur
 
-
+    int liste[2] = {0,1};
     system("cls");
 
 
@@ -259,6 +291,10 @@ int main() {
                 break;
             case 'c':
                 jeu(0);
+                menu();
+                break;
+            case('m'):
+                code();
                 menu();
                 break;
 
